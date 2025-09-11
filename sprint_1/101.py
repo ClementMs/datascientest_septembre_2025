e03d7069a14ea44171f5ee8a0c7bd68558976431
ClementMs <clement.msika@gmail.com>
2025-09-11 23:30:16 +0200

    ajouté deux fonctions

--- sprint_1/101.py
+++ sprint_1/101.py
@@ -45,3 +45,71 @@ def verifier_nombre_premier(nombre):
             return True
         else:
             return False
+
+
+
+
+def afficher_format_texte_équation(dictionnaire):
+    
+    '''
+    Cette fonction affiche sous format texte une équation encodée sous forme de dictionnaire. 
+    Paramètre: dictionnaire. 
+    Exemple: 
+    '''
+    
+    texte = ''
+    
+    for équation in dictionnaire.values():
+        
+        compteur = 0
+        
+        for membre_équation in équation:
+            
+            if compteur == 1:
+                
+                texte += ' = '
+                
+            print(membre_équation)
+            
+            opérateurs = list(membre_équation.keys())[0]
+            variables = list(membre_équation.values())[0]
+            
+            
+        
+            texte += str(variables[0]) 
+            texte = texte + ' ' + str(opérateurs[0])
+            
+            texte = texte + ' ' + str(variables[1]) + ' * x'
+            texte = texte + ' ' + str(opérateurs[1])
+            
+            texte = texte + ' ' + str(variables[2]) + ' * y'
+            texte = texte + ' ' + str(opérateurs[2])
+            
+            texte = texte + ' ' + str(variables[3]) + ' * z'
+            
+            compteur += 1
+            
+    return texte     
+        
+
+def resolution() -> tuple:
+    '''
+    Cette fonction résout le système linéaire à trois équations et trois inconnues x, y et z.
+    Les équations sont documentées sous forme de dictionnaires imbriqués. 
+    Exemple: x + y + z = 2
+    Pour modéliser cette équation, on crée trois dictionnaires. 
+    Le premier dictionnaire associe au nom de l'équation un tuple constitué de deux dictionnaires représentant les deux membres de l'équation.
+    Le deuxième dictionnaire associe au tuple  d'encodage des deux opérateurs qui relient x, y et z (ici ('+','+','+'))  une liste qui encode les quantités des constantes, de x, y et z, ici [0,1,1,1]
+    Le troisième dictionnaire associe au tuple d'encodage des deux opérateurs qui relient x, y et z (ici ('+','+','+')) une liste qui encode les quantités des constantes, de x, y et z, ici [2,0,0,0]
+    Renvoie: 
+    Un tuple constituée des solutions de l'équation de la forme x, y, z
+    '''
+    
+    première_équation_encodée = {}
+    première_équation_encodée_membre_gauche = {}
+    première_équation_encodée_membre_droite = {}
+    première_équation_encodée_membre_gauche[('+','+','+')] = [0,1,1,1]
+    première_équation_encodée_membre_droite[('+','+','+')] = [2,0,0,0]
+    première_équation_encodée['première_équation'] = (première_équation_encodée_membre_gauche, première_équation_encodée_membre_droite)
+    
+    return première_équation_encodée
